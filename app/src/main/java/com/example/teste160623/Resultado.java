6fae01a989ebb97f0a65a9dcc12dbc7d1040c1b0
ifpr <gabrielmoiseis26@gmail.com>
2023-03-30 12:41:43 -0300

    Calcula 40

--- app/src/main/java/com/example/teste160623/Resultado.java
+++ app/src/main/java/com/example/teste160623/Resultado.java
@@ -1,4 +1,34 @@
 package com.example.teste160623;
 
-public class Resultado {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Resultado extends CalculadoraSimples {
+     Button buttonirresultado;
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            View buttonIrResultado = findViewById(R.id.buttonirresultado);
+
+            View buttonirresultado = null;
+            buttonirresultado.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent in;
+                    in = new Intent(CalculadoraSimples.this, Resultado.class);
+//in.putExtra…
+                    startActivity(in);
+                }
+            });
+        }
+
+
+
 }
